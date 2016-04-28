25299403e14ab4e5d9512b6c577929289de26f3d
mmcvay-pivotal <mmcvay@pivotal.io>
2016-04-28 16:49:48 +0100

    Update 2

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	message := os.Getenv("MESSAGE")
 	if message == "" {
-		message = "Hello world mmcvay"
+		message = "Hello world here again"
 	}
 
 	m := martini.Classic()
